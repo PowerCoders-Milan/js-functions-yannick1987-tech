7b14b084d07ae0de02d4792212f66099dc54a1d2
yannick1987-tech <ngassamnanayannick8@gmail.com>
2021-06-16 12:54:06 +0200

    exercise 3 done

--- 03-celsius_fahrenheit/celsius_fahrenheit.js
+++ 03-celsius_fahrenheit/celsius_fahrenheit.js
@@ -1,12 +1,14 @@
 // define a function multiplyByNineFifths(number)
-
+function multiplyByNineFifths(number) {
     //return number*(9/5)
-
+    return number * (9 / 5);
+}
 // define a function getFahrenheit(celsius)
-
+function getFahrenheit(celsius) {
     // return multiplyByNineFifths(celsius) + 32;
-
-console.log() // call getFahrenheit();
-
+    return multiplyByNineFifths(celsius) + 32;
+}
+ // call getFahrenheit();
+console.log(getFahrenheit(10));
 
 // do the same thing starting from Fahrenheit to Celsius
