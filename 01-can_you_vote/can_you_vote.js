81558f726e44d8dce4cd609391ab828b308626a2
yannick1987-tech <ngassamnanayannick8@gmail.com>
2021-06-16 09:52:44 +0200

    exercise 1 done

--- 01-can_you_vote/can_you_vote.js
+++ 01-can_you_vote/can_you_vote.js
@@ -1,15 +1,18 @@
 // define a function that will check if I can vote or not
 const canIVote = age => {
     // define a variable legalAge
-
+legalAge = 18 ;
     // create if else statement that console.log() if you can vote or not
-    if (condition) {
+    if (age <=18) {
+        console.log ('you are too yong to vote ');
         
     } else {
-        
+       console.log ('you may vote');
     }
 }
 
-// define a variable for your age
 
+// define a variable for your age
+ let age = 15;
 // call the function and see if you can vote
+console.log(canIVote(15));
